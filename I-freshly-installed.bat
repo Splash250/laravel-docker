46c62f39c90844eef1c77f8214d7f41b97da2206
Csanád Kémeri <kemeri.csanad@gmail.com>
2024-03-08 10:57:00 +0100

    Fix database stuff

--- I-freshly-installed.bat
+++ I-freshly-installed.bat
@@ -1,5 +1,8 @@
 @echo off
 copy /Y "./.env.example" ".env"
+docker compose up -d --build
 docker compose exec php composer update
 docker compose exec php php artisan key:generate
+docker compose exec php php artisan session:table
+docker compose exec php php artisan migrate
 start http://localhost:8080/
